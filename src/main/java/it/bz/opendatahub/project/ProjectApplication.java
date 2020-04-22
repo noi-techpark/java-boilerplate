7144eb4136c698b97ccf7452e72489c3384244d0
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2020-04-22 12:11:01 +0200

    use PMD java best practices ruleset

--- src/main/java/it/bz/opendatahub/project/ProjectApplication.java
+++ src/main/java/it/bz/opendatahub/project/ProjectApplication.java
@@ -3,6 +3,7 @@ package it.bz.opendatahub.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProjectApplication {
 
